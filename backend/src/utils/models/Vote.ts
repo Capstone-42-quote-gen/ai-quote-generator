dd9ac0823042ea0fc16c1da847f9ae3f507d7765
Manuel Rascon <mrascon19@cnm.edu>
2023-05-25 14:30:47 -0600

    debugged Vote.ts file

--- backend/src/utils/models/Vote.ts
+++ backend/src/utils/models/Vote.ts
@@ -9,7 +9,7 @@ export interface Vote {
 
     export async function insertVote (vote: Vote): Promise<string> {
     const {voteProfileId, votePostId, voteValue} = vote
-        await sql `INSERT INTO  vote (vote_post_id, vote_profile_id, vote_date, vote_value) VALUES(${voteProfileId}, ${votePostId}, NOW(), ${voteValue})`
+        await sql `INSERT INTO  vote (vote_profile_id, vote_post_id, vote_date, vote_value) VALUES(${voteProfileId}, ${votePostId}, NOW(), ${voteValue})`
         return 'Vote created successfully'
     }
 
