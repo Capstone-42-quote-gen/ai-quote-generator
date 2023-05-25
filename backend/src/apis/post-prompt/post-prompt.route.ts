ce7e20f027a326ec3060391ed8c1f749a0567ceb
RSBalchII <128771311+RSBalchII@users.noreply.github.com>
2023-05-25 16:22:33 -0600

    debugged postPrompt.route.ts added check function to postPostPrompt

--- backend/src/apis/post-prompt/post-prompt.route.ts
+++ backend/src/apis/post-prompt/post-prompt.route.ts
@@ -5,15 +5,28 @@ import {
     getPostPromptsByPromptIdController,
     postPostPromptPromptController
 } from "./post-prompt.controller"
+import {asyncValidatorController} from "../../utils/controllers/asyncValidator.controller";
+import {check} from "express-validator";
 
 export const postPromptRoute = Router()
 
 postPromptRoute.route('/postId/:postId/promptId/:promptId')
-    .get(getPostPromptIdByPostIdAndPromptIdController)
-    .post(postPostPromptPromptController)
+    .get (asyncValidatorController([
+        check ('postId', 'please provide a valid postId').isUUID(), check ('promptId', 'please provide a valid promptId').isUUID()
+    ]),getPostPromptIdByPostIdAndPromptIdController)
+
+postPromptRoute.route('/')
+    .post (asyncValidatorController([
+        check ('postId', 'please provide a valid postId').isUUID(), check ('promptId', 'please provide a valid promptId').isUUID()
+    ]),postPostPromptPromptController)
+
 
 postPromptRoute.route('/postId/:PostId')
-    .get(getPostPromptsByPostIdController)
+    .get (asyncValidatorController([
+        check ('postId', 'please provide a valid postId').isUUID()
+        ]),getPostPromptsByPostIdController)
 
 postPromptRoute.route('/promptId/:PromptId')
-    .get(getPostPromptsByPromptIdController)
+    .get (asyncValidatorController([
+        check ('promptId', 'please provide a valid promptId').isUUID()
+    ]),getPostPromptsByPromptIdController)
