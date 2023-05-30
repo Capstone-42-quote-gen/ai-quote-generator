e07ba91c21a340745353583ea5ab875e70ad5755
Gurumustuk Khalsa <gmustuk@gmail.com>
2023-05-30 16:02:57 -0600

    react baseline setup

---
+++ frontend/src/ui/App.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.css'
+import './App.css'
+import { Home } from './Home.tsx'
+import { FourOhFour } from './FourOhFour'
+
+
+export function App() {
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route  path='/' element={<Home />} />
+                    <Route path={"*"} element={<FourOhFour />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
+}
---
+++ frontend/src/ui/FourOhFour.tsx
@@ -0,0 +1,7 @@
+export const FourOhFour = () => {
+    return (
+        <>
+            <h1>This is not the page you were looking for</h1>
+        </>
+    )
+};
