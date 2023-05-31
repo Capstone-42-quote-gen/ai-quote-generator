c785fdd5b55381671cedcbe822bb872f01439371
Manuel Rascon <mrascon19@cnm.edu>
2023-05-31 15:40:37 -0600

    added navbar to home page

--- frontend/src/ui/Home.tsx
+++ frontend/src/ui/Home.tsx
@@ -1,7 +1,10 @@
+import {Navigation} from "./shared_components/NavBar";
+// import logo from "../assets/logo-1.png";
 
 export function Home() {
     return (
         <>
+            <Navigation/>
             <h1>Home</h1>
         </>
     )
