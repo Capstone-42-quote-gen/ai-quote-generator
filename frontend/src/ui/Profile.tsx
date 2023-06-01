7e3b6a527fe742cd1e49fba32d0146c2c8e79a1c
RSBalchII <128771311+RSBalchII@users.noreply.github.com>
2023-06-01 14:55:05 -0600

    completed basic layout of profile page

--- frontend/src/ui/Profile.tsx
+++ frontend/src/ui/Profile.tsx
@@ -1,41 +1,43 @@
 import {Navigation} from "./shared_components/NavBar.tsx";
-import {Col, Container, Row} from "react-bootstrap";
+import {Col, Container, Row, Image} from "react-bootstrap";
 import {ImageOnly} from "./shared_components/imageOnly.tsx";
+import logo from "../assets/logo-1.png";
 
 export function Profile() {
     return (
         <>
 
             <Navigation/>
-            <Container className={''} >
-            <Row md={12}>
-                <Col md={3} className={''} ></Col>
-            </Row>
+            <Container className={'my-3'}>
+                <Row md={12} className={'g-5'}>
+                    <Col>
+                        <Image src={logo} className={'ProfileImage image-fluid'}/>
+                    </Col>
+                </Row>
                 <Row md={12}>
-                <Col md={3} className={''} ></Col>
-            </Row>
+                    <Col md={6}>
+                        <h4>About me</h4>
+                        <p>Something about me.Nullus pisces est maior quam amicitia inter Marlin et Dory. In via sua, per mundum submari, rebus mirabilibus plena, inveniunt multos habitatores marinos.
+                        </p>
+                    </Col>
+                </Row>
                 <Row md={12}>
-                <Col md={3} className={''} ></Col>
-            </Row>
+                    <Col md={3}></Col>
+                </Row>
             </Container>
 
             <Container>
-            <Row className={'g-5'}>
-                <Col md={4} >
-                    <ImageOnly/>
-                </Col>                <Col md={4} >
-                    <ImageOnly/>
-                </Col>                <Col md={4} >
-                    <ImageOnly/>
-                </Col>                <Col md={4} >
-                    <ImageOnly/>
-                </Col>                <Col md={4} >
-                    <ImageOnly/>
-                </Col>                <Col md={4} >
-                    <ImageOnly/>
-                </Col>
+                <Row>
 
-            </Row>
+                </Row>
+            </Container>
+
+            <Container>
+                <Row className={'g-5'}>
+                    <Col md={4}>
+                        <ImageOnly/>
+                    </Col>
+                </Row>
             </Container>
         </>
     )
--- frontend/src/ui/shared_components/ImageContent.tsx
+++ frontend/src/ui/shared_components/ImageContent.tsx
@@ -3,7 +3,7 @@ import redCheck from "../../assets/Red Check.png"
 export function ImageContent() {
     return (
         <>
-            <img className={'ManyImages image-fluid'} src={mountains} alt=""/>
+            <img className={'ManyImages img-fluid border rounded'} src={mountains} alt=""/>
             <button><img className={'redCheckButton image-fluid'} src={redCheck} alt=""/></button>
         </>
     )}
