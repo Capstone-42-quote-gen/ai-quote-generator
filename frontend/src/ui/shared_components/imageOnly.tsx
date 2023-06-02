1322da5a06ae7d4ca22456bf4532d5fb8a277eaa
RSBalchII <128771311+RSBalchII@users.noreply.github.com>
2023-06-01 21:47:53 -0600

    removed unneeded file imageOnly

--- frontend/src/ui/shared_components/imageOnly.tsx
+++
@@ -1,9 +0,0 @@
-import mountains from "../../assets/mountains.png"
-
-export function ImageOnly() {
-    return (
-        <>
-            <img className={'quote-image img-fluid'} src={mountains} alt=""/>
-
-        </>
-    )}
