6220cef721157154d6dded41f453c9272e112a0b
Manuel Rascon <mrascon19@cnm.edu>
2023-06-02 11:27:09 -0600

    added buttons and aligned content

--- frontend/src/ui/shared_components/SignIn.tsx
+++ frontend/src/ui/shared_components/SignIn.tsx
@@ -3,40 +3,54 @@ import {Button, Card, Col, Container, Form, Row} from "react-bootstrap";
 export function SignIn() {
     return (
         <>
-            <Container className={"justify-content-center align-items-center"}>
-                <Row >
-                <Col md={6}>
-            <Card>
-                <Card.Body>
-                    <Card.Title className="text-center py-4">
-                        <h1>Sign In</h1>
-                    </Card.Title>
-            <Form>
-                <Form.Group className="mb-3" controlId="formHorizontalEmail">
-                    <Form.Label column lg={2} className="ms-2">
-                        Email
-                    </Form.Label>
-                    <Col lg={12}>
-                        <Form.Control type="email" placeholder="Email"/>
+            <Container className={"align-items-center py-3"}>
+                <Row className={"justify-content-center"}>
+                    <Col md={6}>
+                        <Card>
+                            <Card.Body>
+                                <Card.Title className="text-center py-4">
+                                    <h1>Sign In</h1>
+                                </Card.Title>
+                                <Form>
+                                    <Form.Group className="mb-3" controlId="formHorizontalEmail">
+                                        <Form.Label column lg={2} className="ms-2">
+                                            Email
+                                        </Form.Label>
+                                        <Col lg={12}>
+                                            <Form.Control type="email" placeholder="Email"/>
+                                        </Col>
+                                    </Form.Group>
+                                    <Form.Group as={Row} className="mb-3" controlId="formHorizontalPassword">
+                                        <Form.Label column lg={4} className="ms-2">
+                                            Password
+                                        </Form.Label>
+                                        <Col lg={12}>
+                                            <Form.Control type="password" placeholder="Password"/>
+                                        </Col>
+                                    </Form.Group>
+                                    <Form.Group as={Row} className="mb-3">
+                                        <Col lg={"12"}>
+                                            <Button variant={"secondary"} type={"submit"}>Sign in</Button>
+                                        </Col>
+                                    </Form.Group>
+                                </Form>
+                            </Card.Body>
+                        </Card>
                     </Col>
-                </Form.Group>
-                <Form.Group as={Row} className="mb-3" controlId="formHorizontalPassword">
-                    <Form.Label column lg={4} className="ms-2">
-                        Password
-                    </Form.Label>
-                    <Col lg={12}>
-                        <Form.Control type="password" placeholder="Password"/>
+                </Row>
+                <Row className={"justify-content-center"}>
+                    <Col lg={8}>
+                        <div className={"text-center"}>
+                            <span className={"divider-text"}><hr/>Not signed up yet?<hr/></span>
+                        </div>
                     </Col>
-                </Form.Group>
-                <Form.Group as={Row} className="mb-3">
-                    <Col lg={"12"}>
-                        <Button variant={"secondary"} type={"submit"}>Sign in</Button>
+                </Row>
+                <Row className={"justify-content-center"}>
+                    <Col lg={8}>
+                    <div className={"d-grid"}>
+                        <Button variant={"secondary"} size={"lg"}>Click here</Button>
+                    </div>
                     </Col>
-                </Form.Group>
-            </Form>
-                </Card.Body>
-            </Card>
-                </Col>
                 </Row>
             </Container>
         </>
