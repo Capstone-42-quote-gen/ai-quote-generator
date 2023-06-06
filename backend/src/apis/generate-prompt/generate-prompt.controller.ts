b8067cd07a7b65da20cc6bd97fb7ccdbf55bba6a
Manuel Rascon <mrascon19@cnm.edu>
2023-06-06 11:04:05 -0600

    added generateImage function to unsplash.utils.ts file

--- backend/src/apis/generate-prompt/generate-prompt.controller.ts
+++ backend/src/apis/generate-prompt/generate-prompt.controller.ts
@@ -1,14 +1,19 @@
 import {Request, Response} from "express";
 import { generatePrompt } from "../../utils/openai.utils";
+import {generateImage} from "../../utils/unsplash.utils";
+
 
 export async function generatePromptController(request: Request, response: Response) {
 
     try {
         const { topicValue, voiceValue } = request.body;
         console.log(topicValue, voiceValue)
-        const completion = await generatePrompt(topicValue, voiceValue);
+        // const completion = await generatePrompt(topicValue, voiceValue);
+        const completion = await generateImage(topicValue);
+        console.log(completion)
         return response.json({ status: 200, message: "success", data: completion });
     } catch (error) {
         response.json({ status: 500, message: 'Error generating prompt', data: null });
     }
 }
+
--- backend/src/utils/unsplash.utils.ts
+++ backend/src/utils/unsplash.utils.ts
@@ -1,32 +1,61 @@
 import {createApi} from "unsplash-js";
-
-type Photo = {
-    id: number;
-    width: number;
-    height: number;
-    urls: { large: string; regular: string; raw: string; small: string };
-    color: string | null;
-    user: {
-        username: string;
-        name: string;
-    };
-};
+import * as nodeFetch from 'node-fetch';
 
 // @ts-ignore
-const serverApi = createApi({
+const unsplash = createApi({
     accessKey: process.env.UNSPLASH_API_KEY,
+    fetch: nodeFetch.default as unknown as typeof fetch,
 });
 
-const Body: FC = () => {
-    const [data, setPhotosResponse] = useState(null);
+export async function generateImage(topic: string): Promise<any> {
+    console.log("unsplash", unsplash)
+    console.log("process.env", process.env)
+    unsplash.search.getPhotos({query: topic})
+        .then(result => {
+        if (result.errors) {
+            // handle error here
+            console.log('error occurred: ', result.errors[0]);
+        } else {
+            const feed = result.response;
 
-    useEffect(() => {
-        api.search
-            .getPhotos({ query: "cat", orientation: "landscape" })
-            .then(result => {
-                setPhotosResponse(result);
-            })
-            .catch(() => {
-                console.log("something went wrong!");
-            });
-    }, []);
+            // extract total and results array from response
+            const {total, results} = feed;
+
+            // handle success here
+            console.log(`received ${results.length} photos out of ${total}`);
+            console.log('first photo: ', results[0]);
+        }
+    });
+}
+
+// type Photo = {
+//     id: number;
+//     width: number;
+//     height: number;
+//     urls: { large: string; regular: string; raw: string; small: string };
+//     color: string | null;
+//     user: {
+//         username: string;
+//         name: string;
+//     };
+// };
+
+// @ts-ignore
+// const serverApi = createApi({
+//     accessKey: process.env.UNSPLASH_API_KEY,
+// });
+
+
+// const Body: FC = () => {
+//     const [data, setPhotosResponse] = useState(null);
+
+    // useEffect(() => {
+    //     serverApi.search
+    //         .getPhotos({query: topic, orientation: "landscape"})
+    //         .then(result => {
+    //             return result;
+    //         })
+    //         .catch(() => {
+    //             console.log("something went wrong!");
+    //         });
+    // }, []);
