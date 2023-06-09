cea39868978850714a2572b2ee6e46e12a164431
RSBalchII <128771311+RSBalchII@users.noreply.github.com>
2023-06-09 12:05:41 -0600

    adding store files required to interact with apis.ts

---
+++ frontend/src/store/profile.ts
@@ -0,0 +1,23 @@
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+
+
+interface ProfileState {
+    [profileId: string]: any;
+}
+
+const slice = createSlice({
+    name: "profiles",
+    initialState: {} as ProfileState,
+    reducers: {
+        setProfile: (profiles, action: PayloadAction<{ profileId: string, data: any }>) => {
+            profiles[action.payload.profileId] = action.payload.data;
+        }
+    }
+});
+
+export const { setProfile } = slice.actions;
+
+export const fetchProfileByProfileId = (profileId: string) => {
+    const state = getState();
+    const profiles
+}
