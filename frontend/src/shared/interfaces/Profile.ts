e7c490b3382b9a9b0f01309691a6f35d14f825e5
RSBalchII <128771311+RSBalchII@users.noreply.github.com>
2023-06-09 10:45:44 -0600

    updated auth and hook files

---
+++ frontend/src/shared/interfaces/Profile.ts
@@ -0,0 +1,8 @@
+
+export interface Profile {
+    profileId: string | null ,
+    profileUsername: string,
+    profilePassword: string,
+    profilePhotoUrl: string,
+    profileEmail: string,
+}
--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,8 +1,10 @@
 
 import { apis } from './apis'
 import {setupListeners} from "@reduxjs/toolkit/query";
-import {configureStore} from "@reduxjs/toolkit";
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
+import {TypedUseSelectorHook, useDispatch} from "react-redux";
 
+const reducer = combineReducers({auth, votes, currentUser, posts, profiles, api: apis.reducer})
 export const store = configureStore({
     reducer: {
         [apis.reducerPath]: apis.reducer
@@ -11,3 +13,10 @@ export const store = configureStore({
 })
 
 setupListeners(store.dispatch)
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+
+// useAppDispatch and useAppDelectore are custom hooks that allow us to use redux in our components
+export const useAppDispatch: () => AppDispatch = useDispatch
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
