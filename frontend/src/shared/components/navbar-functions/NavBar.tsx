807a863bad96072cda723465582e7884bfd4accf
Manuel Rascon <mrascon19@cnm.edu>
2023-06-13 18:42:54 -0600

    added route to popular in navbar

--- frontend/src/shared/components/navbar-functions/NavBar.tsx
+++ frontend/src/shared/components/navbar-functions/NavBar.tsx
@@ -20,7 +20,7 @@ export function Navigation() {
                 {/*    <Navbar.Toggle aria-controls={"responsive-navbar-nav"} />*/}
                     {/*<Navbar.Collapse id={"responsive-navbar-nav"}>*/}
                     <Nav className={"w-100 justify-content-evenly"}>
-                        <Nav.Link href="#popular">Popular</Nav.Link>
+                        <Nav.Link href="/popular">Popular</Nav.Link>
                         <span className={"navbar-divider"}></span>
                         <Nav.Link href="/new">New</Nav.Link>
                         <span className={"navbar-divider"}></span>
