af744a3664f3455db5eac0fab08d954e6f733b6e
Gurumustuk Khalsa <gmustuk@gmail.com>
2023-06-25 15:42:55 -0600

    removed react helmut since not working for social share og:image for facebook share

--- frontend/src/shared/components/HomeGallery/GalleryContent.tsx
+++ frontend/src/shared/components/HomeGallery/GalleryContent.tsx
@@ -3,7 +3,6 @@ import img_share from "/src/assets/share.png";
 import img_heart_0 from "/src/assets/heart-0.png";
 import img_heart_1 from "/src/assets/heart-1.png";
 import img_download from "/src/assets/download.png";
-import {Helmet} from 'react-helmet';
 import {Link} from "react-router-dom";
 import {Post} from "../../interfaces/Post";
 import {
@@ -70,11 +69,7 @@ export function GalleryContent(props: GalleryContentProps) {
 
     return (
         <>
-            <Helmet>
-                <meta property="og:image" content={post.postPhotoUrl}/>
-                <meta property="og:image:alt" content={decodedPostQuote}/>
-                <meta property="og:image:width" content="1080"/>
-            </Helmet>
+
 
 
             <div className="d-flex justify-content-center rounded my-4">
