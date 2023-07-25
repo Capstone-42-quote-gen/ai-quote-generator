f685071615353bc7d7f571b9f8bc6dbf8958fc7a
Gurumustuk Khalsa <gmustuk@gmail.com>
2023-07-25 17:56:24 -0600

    updated openAI model to chat completions and gpt4 and added updated simpler prompt for efficiency and humor

--- backend/src/utils/openai.utils.ts
+++ backend/src/utils/openai.utils.ts
@@ -5,76 +5,62 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-export async function generatePrompt(topic: string, voice: string): Promise<string> {
-    const prompt =
-`From now on you are a de-motivational comedian and the responses are your thoughts.
-You are very sarcastic and you must make the quote funny.
-Your Humor appeals to Millennial and Gen Z people.
+export async function generatePrompt(topic: string, voice: string): Promise<string | null> {
 
-How should you respond?
-I will give you a TOPIC to use each time.
-I will give you a VOICE to use each time.
-Provide me only a single de-motivational quote using both the voice and topic.
+    const prompt = `As an AI adopting the persona of a quick-witted comedian known for subtly de-motivating humor, your task is to come up with a witty, amusing quote within 160 characters that is centered on a given topic. Remember to make the topic the main focus of the humor without openly referencing it or the voice. Use the voice to subtly influence the delivery style of the quote. Refrain from providing personal opinions about the user.
 
-How should you not respond?
-Do not provide personal opinions or assumptions about the user.
-Do not declare the voice or the topic before saying it.
-You only respond with the quote. You say nothing else.
-
-What type of information do I want?
-Provide only factual interpretations based on the information given.
-List the voice after the quote every time.
-The quote needs to mimic the voice explicitly.
+Ensure the quote is well thought-out and the humor is subtle and clever. The quote should follow this format: "Quote text" - Voice.
 
 TOPIC: ${topic}
 VOICE: ${voice}
 `;
 
-try {
-
-    const completion = await openai.createCompletion({
-        model: 'text-davinci-003',
-        prompt: prompt,
-        temperature: 1.2,
-        max_tokens: 50,
-    });
-
-    const choice = completion.data.choices[0];
-    const generatedQuote = choice?.text?.trim();
-
-    return generatedQuote || "";
-} catch (error) {
-    throw new Error("Failed to generate prompt");
-}
-}
 
 
-// METHOD USING CHAT COMPLETIONS
+        try {
+            const chatCompletion = await openai.createChatCompletion({
+                model: 'gpt-4',
+                messages: [{ role: 'user', content: prompt }],
+                temperature: 1.2,
+                max_tokens: 40
+            });
+
+
+            if (chatCompletion.data.choices.length > 0) {
+                const choice = chatCompletion.data.choices[0];
+                return choice.message?.content ?? null;
+            }
+
+            return null;
+
+        } catch (error) {
+            console.error('Failed to generate prompt', error);
+            return null;
+        }
+
+    }
+
+
+// Older completion method from older model
+
 // try {
-//     const chatCompletion = await openai.createChatCompletion({
-//         model: 'gpt-3.5-turbo',
-//         messages: [{role: "user", content: prompt}],
+//
+//     const completion = await openai.createCompletion({
+//         model: 'text-davinci-003',
+//         prompt: prompt,
 //         temperature: 1.2,
-//         max_tokens: 40,
+//         max_tokens: 50,
 //     });
+//     console.log(completion)
 //
-//     // Check if choices array exists and has at least one element
-//     if (chatCompletion.data.choices && chatCompletion.data.choices.length > 0) {
-//         const firstChoice = chatCompletion.data.choices[0];
 //
-//         // Check if the 'message' property exists in the choice
-//         if (firstChoice && firstChoice.message) {
-//             const messageContent = firstChoice.message.content;
 //
-//             console.log(messageContent);
-//             return messageContent;
-//         }
-//     }
 //
-//     console.error("No choices or messages found in the response.");
-//     return null;
+//     const choice = completion.data.choices[0];
+//     const generatedQuote = choice?.text?.trim();
+//
+//     return generatedQuote || "";
 // } catch (error) {
-//     console.error("Failed to generate prompt. Error:", error);
-//     return null;
+//     throw new Error("Failed to generate prompt");
 // }
 // }
